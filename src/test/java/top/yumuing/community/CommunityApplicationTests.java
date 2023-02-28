c5e497d21b20f78584a715287329afe73935246a
yumuing <yuzhe780@126.com>
2023-02-28 16:08:44 +0800

    日志相关的项目调试

--- src/test/java/top/yumuing/community/CommunityApplicationTests.java
+++ src/test/java/top/yumuing/community/CommunityApplicationTests.java
@@ -6,7 +6,6 @@ import org.springframework.beans.BeansException;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.ApplicationContextAware;
-import org.springframework.stereotype.Repository;
 import org.springframework.test.context.ContextConfiguration;
 import top.yumuing.community.entity.DiscussPost;
 import top.yumuing.community.entity.User;
---
+++ src/test/java/top/yumuing/community/LoggerTest.java
@@ -0,0 +1,26 @@
+package top.yumuing.community;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+@ContextConfiguration(classes = CommunityApplication.class)
+public class LoggerTest {
+
+    private static final Logger logger = LoggerFactory.getLogger(LoggerTest.class);
+
+    @Test
+    public void testLogger(){
+        System.out.println(logger.getName());
+        logger.info("info");
+        logger.warn("warn");
+        logger.error("error");
+    }
+
+}
